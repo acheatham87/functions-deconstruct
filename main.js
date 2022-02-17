1672712495392a6a848d0d79f99ee527fe5c2a48
acheatham87 <acheatham87@gmail.com>
2022-02-17 13:15:25 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,81 @@
+// Here's what I need to do.
+
+// Get a piece of A3 paper
+// Fold it in half
+// Clean my fountain pen
+// Fill fountain pen with ink color I want
+// Write letter
+// Fold letter to make it into an envelope to be addressed
+// Melt wax colors I want for the seal
+// Pour wax onto letter
+// Press seal into wax
+// Address the envelope
+// Add stamp to envelope
+// Mail letter
+// Your job is to deconstruct your hobby, job, or craft into the tasks required.
+
+// Write a function for each task.
+// In the function body, just console.log() a description of the task.
+// Invoke the tasks in the correct order.
+
+
+GetPaper = () => {
+    console.log("Get a piece of A3 paper.")
+}
+
+foldPaper = () => {
+    console.log("Fold paper in half.")
+}
+
+cleanPen = () => {
+    console.log("Clean my fountain pen.")
+}
+
+fillPen = () => {
+    console.log("Fill fountain pen with ink color I want.")
+}
+
+writeLetter = () => {
+    console.log("Write letter.")
+}
+
+foldLetter = () => {
+    console.log("Fold letter to make it into an envelope to be addressed.")
+}
+
+meltWax = () => {
+    console.log("Melt wax colors I want for the seal.")
+}
+
+pourWax = () => {
+    console.log("Pour wax onto the letter.")
+}
+
+pressSeal = () => {
+    console.log("Press seal into wax.")
+}
+
+addressEnvelope = () => {
+    console.log("Address the envelope.")
+}
+
+stampEnvelope = () => {
+    console.log("Add stamp to envelope.")
+}
+
+mailLetter = () => {
+    console.log("Mail letter.")
+}
+
+GetPaper()
+foldPaper()
+cleanPen()
+fillPen()
+writeLetter()
+foldLetter()
+meltWax()
+pourWax()
+pressSeal()
+addressEnvelope()
+stampEnvelope()
+mailLetter()
